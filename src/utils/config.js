ffa1b8273ad767784577fc9fd96d62464070b742
Hamza Göksal <hgoksal@sutas.com.tr>
2025-01-26 02:48:14 +0300

    Update config URLs with minor formatting adjustments

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,6 @@
 export const Config = {
     ENV: "Dev",
     VERSION: "1.0.0",
-    API_URL: "https://api.fitjourney.com.tr/api",
-    CDN_URL: "https://cdn.fitjourney.com.tr",
+    API_URL:"https://api.fitjourney.com.tr/api",
+    CDN_URL:"https://cdn.fitjourney.com.tr",
 }
